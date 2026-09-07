0fafac8aca2c3c4542c33156b0e0dbbc54b4df75
justmeFRUZ <justme777666@gmail.com>
2026-09-07 04:23:06 +0300

    add load/save functions for JSON persistence

--- task_manager.py
+++ task_manager.py
@@ -1,1 +1,19 @@
-print("Task Manager")
+import json
+import os
+
+TASKS_FILE = "tasks.json"
+
+def load_tasks():
+    if os.path.exists(TASKS_FILE):
+        with open(TASKS_FILE, "r") as f:
+            return json.load(f)
+
+    else:
+        return []
+
+
+def save_tasks(tasks):
+    with open(TASKS_FILE, "w") as f:
+        json.dump(tasks, f, indent=4)
+
+
